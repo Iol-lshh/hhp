3dbe904be0d607b34ec454701a9a4d51865d0555
lshh <markhong93@naver.com>
2024-01-05 01:32:50 +0900

    동시성 테스트.. H2는 PESSIMISTIC_WRITE 락을 못한다고 한다.. hsqldb로 옮겨보자....

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -55,30 +55,53 @@ CREATE INDEX purchase_product_id ON tb_order_item(product_id, count);
 CREATE INDEX purchase_user_id ON tb_order_item(user_id);
 
 --
-CREATE VIEW v_point AS
-select
-    user_id,
-    sum(p.count) as remain
-from tb_point p
-group by user_id;
+-- CREATE VIEW v_point AS
+-- select
+--     user_id,
+--     sum(p.count) as remain
+-- from tb_point p
+-- group by user_id;
 
 --
-CREATE VIEW v_top_purchased_product AS
-with sum_paid_cnt AS (
-    select
-        tpc.product_id,
-        sum(tpc.count) as paid_cnt
-    from tb_order_item tpc
-    group by tpc.product_id
-)
-select
-    tp.*,
-    spc.paid_cnt,
-    row_number() over(order by spc.paid_cnt desc) as order_by_paid_cnt
-from sum_paid_cnt spc
-inner join tb_product tp
-    on tp.id = spc.product_id
-order by paid_cnt desc;
+-- CREATE VIEW v_top_purchased_product AS
+-- with sum_paid_cnt AS (
+--     select
+--         tpc.product_id,
+--         sum(tpc.count) as paid_cnt
+--     from tb_order_item tpc
+--     group by tpc.product_id
+-- )
+-- select
+--     tp.*,
+--     spc.paid_cnt,
+--     row_number() over(order by spc.paid_cnt desc) as order_by_paid_cnt
+-- from sum_paid_cnt spc
+-- inner join tb_product tp
+--     on tp.id = spc.product_id
+-- order by paid_cnt desc;
+-- CREATE TEMPORARY TABLE sum_paid_cnt AS
+-- SELECT
+--     tpc.product_id,
+--     sum(tpc.count) AS paid_cnt
+-- FROM
+--     tb_order_item tpc
+-- GROUP BY
+--     tpc.product_id;
+--
+-- CREATE VIEW v_top_purchased_product AS
+-- SELECT
+--     tp.*,
+--     spc.paid_cnt,
+--     ROW_NUMBER() OVER(ORDER BY spc.paid_cnt DESC) AS order_by_paid_cnt
+-- FROM
+--     sum_paid_cnt spc
+--         INNER JOIN
+--     tb_product tp
+--     ON
+--         tp.id = spc.product_id
+-- ORDER BY
+--     paid_cnt DESC;
+
 
 /*----------------------------------------*/
 
