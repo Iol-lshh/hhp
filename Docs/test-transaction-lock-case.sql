5a50032459fb4f92c70f4af58a5aa1215754365b
lshh <markhong93@naver.com>
2024-01-06 04:14:51 +0900

    title: 동시성 문제 해결 (2번 시도) - 비관적 락은 락을 얻는데만 사용하고, - 실제 데이터 가져오는건 그 다음에 비관적 락 없이 조회하자!
    todo: 확인 내용을 캡쳐하고 문서화 및 정리할 것!
      desc: 4 * 3 * 3 종 테스트 결과
      desc2:
        - 트랜잭션 레벨:
          1. serializable
          2. repeatable read
          3. read committed
          4. read uncommitted
        - 락
          1. 비관적 락
          2. update
          3. insert
        - 디비 엔진
          1. H2 (api 앱 로그)
          2. PostgreSQL (쿼리 편집기)
          3. MySQL (쿼리 편집기)

--- Docs/test-transaction-lock-case.sql
+++ Docs/test-transaction-lock-case.sql
@@ -24,7 +24,7 @@ begin transaction
 --isolation level serializable 
 --isolation level repeatable read
 --isolation level read committed 
-isolation level read uncommitted 
+isolation level read uncommitted
 
 --read write
 --read only
@@ -52,17 +52,25 @@ commit
  * # 포스트그레
  * ## 비관적 락 모드
  * 트랜잭션 레벨과 관계없이, 
- * 우선적으로 비관 모드의 락을 획득한 트랜잭션이 종료될 때 까지, 
- * 대기하고, 쿼리를 진행
+ * 우선적으로 '비관적 락 모드의 쿼리(select)'를 실행한 트랜잭션이 종료될 때 까지,
+ * 대기만 하고, 그대로 그 '비관적 락 모드의 쿼리(select)'를 진행
  * 이후의 쿼리 진행은 커밋 시점까지 트랜잭션 격리 레벨을 따른다.
+
+ - 왜 '비관적' 락일까?
+    - 비관적이라서,, 회계사가 비관적으로 생각하듯이,, 안될껄 먼저 생각해서.. 충돌날꺼라고 생각하고 만드는거라
+    - 즉, 충돌날껄 기다려서 row를 가져오는 것일뿐이다. (row는 이미 뭐 가져올지 골랐고, 락이 걸려있으니 **건져내는 것**을 **기다리는 것**일 뿐이다!!!!!!!! (update야 건져낼때 문제 없지만, insert시 문제, delete는...? 확인해보자... # todo - delete 확인)
+        - todo 같은 비관락이 아니면, insert update 다 할 수 있는 듯 했는데.. 이 부분도 다시 확인하자..
  *
- * 비관적 락을 거는 조회 쿼리는, 락 획득이 목적이 되었을때, 비로소 아름답다..
+ * 비관적 락을 거는 조회 쿼리는, 락 획득이 목적이 되었을때, 비로소 생각대로 진행되었다..
  * 결론) 비관적 락을 거는 조회 쿼리는, 조회를 목적으로 하면 안될 것 같다...!
  *
 
  동시성 문제 해결 (2번 시도)
 - 비관적 락은 락 얻는데만 사용하고,
 - 실제 데이터 가져오는건 그 다음에 비관적 락 없이 조회하자!
+ # todo - 확인 내용을 캡쳐하고 문서화 및 정리할 것! - 트랜잭션 레벨( 1. serializable  2. repeatable read  3. read committed   4. read uncommitted ) * 락( 1. 비관적 락  2. update  3. insert ) * 디비 엔진( 1. H2  2. PostgreSQL  3. MySQL ) = 4 * 3 * 3 종 테스트 결과 작성
+
+
 
  * ## 리드 언커밋
  * 트랜잭션 시작 시점부터 있는 row만 읽을 수 있다.
